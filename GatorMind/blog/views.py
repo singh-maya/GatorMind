24c777b397b5fd438aec63b7d218fe70a343ac91
mackenziehirsh <mackenziehirsh@ufl.edu>
2022-10-31 19:35:18 -0400

    All Django files with added code for Category, Comment, and Post models

---
+++ GatorMind/blog/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+
+from blog.forms import CommentForm
+from blog.models import Post, Comment
+
+
+def blog_index(req):
+    posts = Post.objects.all()
+    context = {
+        "posts":posts,
+    }
+    return render(req, "blog_index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories_name_contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+    }
+
+    return render(request, "blog_detail.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+    comments = Comment.objects.filter(post=post)
+    context = {
+        "post": post,
+        "comments": comments,
+        "form": form,
+    }
+    return render(request, "blog_detail.html", context)
+# Create your views here.
