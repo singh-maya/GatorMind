1248bb0f3e6940c5a1d44b183b5fd868e2fec467
singh-maya <87378136+singh-maya@users.noreply.github.com>
2022-11-02 17:21:01 -0400

    backend

---
+++ backend/blog/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import *
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'body', 'created_on', 'last_modified', 'categories']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['author', 'body', 'created_on', 'post']
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['first_name', 'last_name', 'email', 'username', 'password']
