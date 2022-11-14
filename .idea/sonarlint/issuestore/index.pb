9a3284c73c20938f215f25d66403563181aec86b
singh-maya <singh.maya@ufl.edu>
2022-11-14 10:49:44 -0500

    added settings page frontend

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,31 @@
 
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+W
+'frontend/src/components/Notification.js,5/b/5bac041c9e9e097d4a20f4358160b00ba148ffb5
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+=
+frontend/.env,e/5/e51ccb9a6155f3c59685c017c8fce22c869af68e
+M
+frontend/src/pages/Default.js,2/d/2d717aca681900dbfeb6cb7713904a27ad202aa2
+E
+frontend/src/index.js,0/9/09a4e53ab0e5e7637fd01136f19a9bb570e1ef2d
+O
+frontend/src/pages/LoginPage.js,0/8/08ce1c4404700c0f936d6d8efb49e5674fb8e324
+M
+frontend/src/actions/types.js,d/7/d70edd6fd970b89db492e3ab7acdbb6d749d450e
+J
+frontend/src/pages/Home.js,6/d/6d69fb10eaae9b9c3ab35dc11a291465f02da4fe
+M
+frontend/src/pages/Profile.js,1/c/1c9645c2453bd40c906b8a112052b43f18c4d5e2
+E
+frontend/package.json,3/7/37083060967a471c011a16651e22bdc2ade41759
+J
+frontend/package-lock.json,7/5/75b3b81cff849b08343553e269c3327a04ab22e0
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+J
+frontend/public/index.html,5/3/53b461857fde9b76a1c6e79a500050ceffbd7f33
--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,6 +1,80 @@
+import {Col, Container, Row} from "react-bootstrap";
+import {CircleFill, Gear} from "react-bootstrap-icons";
+import "./Settings.css";
 const Settings = () => {
     return (
-        <h1>Settings Page</h1>
+        <div className="d-grid gap-md-3">
+            <div className="p-3">
+            </div>
+            <div className="container">
+                <div className="row">
+
+                    <div className="col col-sm-4">
+                        <div className="p-2">
+                            <div className="row align-items-start">
+                                <div className="col">
+                                    <center>
+                                        <CircleFill size={70}/>
+                                    </center>
+                                </div>
+                                <div className="col">
+                                    <h2>"NAME"</h2>
+                                </div>
+                            </div>
+                            <center>
+                                <div className='d-grid gap-md-3'>
+                                    <div className="p-1">
+                                    </div>
+                                    <div className="row align-items-center">
+                                        <div className="col">
+                                            <button className="btn btn-primary">
+                                                Followers:
+                                            </button>
+                                        </div>
+                                        <div className="col">
+                                            <button className="btn btn-primary">
+                                                Following:
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <button className="btn btn-secondary">
+                                        <Gear />
+                                        Settings
+                                    </button>
+                                    <p className="lead">Total Posts: </p>
+                                    <p className="lead">Total Likes: </p>
+                                    <p className="lead">Total Comments: </p>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="p-2 bg-light border">
+                            <p className="h2">Settings</p>
+                            <div className="d-grid gap-md-3">
+                                <div className="p-1">
+                                </div>
+                                <div className="p-2 bg-white border">
+                                    <p className="h5">Account Information</p>
+                                    <div className="row">
+                                        <div className="col">
+                                            <p className="h7">Email:</p>
+                                        </div>
+                                        <div className="col">
+                                            <p className="h7">Name:</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="p-2 bg-white border">
+                                    <p className="h5">Other Settings</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
