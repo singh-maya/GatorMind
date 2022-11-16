ee1d5ce2723449f9d66ea3e0bf549813bc80a797
Meryem Yuksel <myuksel@ufl.edu>
2022-11-15 22:35:23 -0500

    Updated version of Post and PostList

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,27 +1,46 @@
-const Post = () =>{
-    const { id } = useParams();
-    const { data: blog, error, isPending } =  useFetch('http://localhost:8000/post/' + id);
-    const history = useHistory();
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-    const handleClick = () => {
-        fetch('http://localhost:8000/post/'+ blog.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/');
-        })
-    }
+const Post = (props) => {
+    const [blog, setBlog] = useState({});
+
+    useEffect(() => {
+        const slug = props.match.params.id;
+
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/blog/${slug}`);
+                setBlog(res.data);
+            }
+            catch (err) {
+
+            }
+        };
+
+        fetchData();
+    }, [props.match.params.id]);
+
+    const createBlog = () => {
+        return {__html: blog.content}
+    };
+
+    const capitalizeFirstLetter = (word) => {
+        if (word)
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        return '';
+    };
 
     return (
-        <div className="blog-details">
-            {isPending && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            <article >
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-                <div>{blog.body}</div>
-                <button onClick={handleClick}>Delete</button>
-            </article>
+        <div className='container mt-3'>
+            <h1 className='display-2'>{blog.title}</h1>
+            <h2 className='text-muted mt-3'>Category: {capitalizeFirstLetter(blog.category)}</h2>
+            <h4>{blog.month} {blog.day}</h4>
+            <div className='mt-5 mb-5' dangerouslySetInnerHTML={createBlog()} />
+            <hr />
+            <p className='lead mb-5'><Link to='/blog' className='font-weight-bold'>Back to Blogs</Link></p>
         </div>
     );
-}
+};
+
 export default Post;
