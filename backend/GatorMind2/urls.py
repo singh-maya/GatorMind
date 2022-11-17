c732c0b130e25aadbe2173e663bba966a053a86d
singh-maya <singh.maya@ufl.edu>
2022-11-16 21:55:36 -0500

    changed package.json and package-lock.json (might cause conflict in future)

--- backend/GatorMind2/urls.py
+++ backend/GatorMind2/urls.py
@@ -20,24 +20,7 @@ from rest_framework import routers, serializers, viewsets
 from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'password','is_staff']
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
-    path('users/', include(router.urls)),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
